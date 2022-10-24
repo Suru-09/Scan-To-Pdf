94a57e2aef2593dc4dcbf7c4b05043ab52590270
Suru <dragos.surugiu13@yahoo.com>
2022-10-24 23:52:22 +0300

    Added structure for the database calls

---
+++ easyPdf/db/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Document(models.Model):
+    user_fk = models.ForeignKey(User, null=True, on_delete=models.CASCADE, db_column='user_id')
+    name = models.CharField(max_length=200, null=False)
+    date = models.DateTimeField(auto_now_add=True, null=True)
+    size = models.IntegerField(null=False, default=0)
+
+
+class Image(models.Model):
+    name = models.CharField(max_length=200, null=False)
+    image = models.ImageField(null=True, blank=True, upload_to="images/")
+    order_no = models.IntegerField(null=False, default=0)
+    size = models.IntegerField(null=False, default=0)
+    document_fk = models.ForeignKey(Document, null=True, on_delete=models.CASCADE, db_column='document_id')
