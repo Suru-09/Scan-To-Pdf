8b38069ee715fd7a463c271d62a37d05e33aee75
Cristina <stiopucristina@yahoo.com>
2022-11-01 23:52:00 +0200

    Adding screen views, trying to setup emulator

---
+++ easyPdf/frontend/screens/CaptureScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from React
+
+export default class Capture extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>App starts here</Text>
+            </View>
+        );
+    }
+}
