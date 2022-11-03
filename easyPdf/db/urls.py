37bc0a7f17737928f43d76d3fc3437bf2c3ffca5
Suru <dragos.surugiu13@yahoo.com>
2022-11-03 22:12:59 +0200

    Added different arrays for urls

--- easyPdf/db/urls.py
+++ easyPdf/db/urls.py
@@ -1,13 +1,28 @@
 from django.urls import path, include
+
+# users
 from .views.userViews import getUsersCreateUser
+
+# documents
 from .views.documentsViews import getDocsCreateDoc
+
+# images
 from .views.imagesViews import getImagesCreateImage
 
 user_patterns = [
     path('', getUsersCreateUser),
 ]
+
+document_patterns = [
+    path('', getDocsCreateDoc),
+]
+
+images_patterns = [
+    path('', getImagesCreateImage)
+]
+
 urlpatterns = [
     path('users', include(user_patterns)),
-    path('documents', getDocsCreateDoc),
-    path('images', getImagesCreateImage)
+    path('documents', include(document_patterns)),
+    path('images', include(images_patterns))
 ]
