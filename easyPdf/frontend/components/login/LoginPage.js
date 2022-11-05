a12784ffd595d95f9014bdb31386f8cbb1f25667
Cristina <stiopucristina@yahoo.com>
2022-11-05 23:06:03 +0200

    Minimum changes login, trying to style

--- easyPdf/frontend/components/login/LoginPage.js
+++ easyPdf/frontend/components/login/LoginPage.js
@@ -1,24 +1,29 @@
 import React from "react";
-import { VStack } from 'react-native-flex-layout';
+import {HStack, VStack} from 'react-native-flex-layout';
 import { IconComponentProvider, Icon, Button  } from "@react-native-material/core";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
-import { TextInput, IconButton } from "@react-native-material/core";
+import { TextInput, IconButton, Text } from "@react-native-material/core";
 import flex from "react-native-flex-layout/src/Flex";
 
 const LoginPage = () => {
     return(
         <VStack spacing={30} style={{ marginHorizontal:"10%",  justifyContent: "center", alignItems: "center", display:"flex", maxWidth:"80%", marginTop:"40%", marginBottom:"40%" }} >
-            <Icon name="home" size={35} color="black" />
+            <Icon name="account" size={35} color="black" />
             <TextInput variant="outlined" label="username" style={{ width:"100%" }} />
             <TextInput
               label="password"
               variant="outlined"
               style={{ width:"100%" }}
               trailing={props => (
-                <IconButton icon={props => <Icon name="eye" {...props} />} {...props} />
+                <IconButton icon={props => <Icon name="key" {...props} />} {...props} />
               )}
             />
-            <Button title="Login" />
+            <Button title="Login" uppercase={false} color="blue" />
+            <HStack spacing={3} style={{ maxHeight:"10%"}}>
+                <Text variant="subtitle2" style={{ marginTop:"3%" }}>Don't have an account?</Text>
+                <Button variant="text" title="Sign Up" color="blue" uppercase={false} compact={true} style={{ height:"10%" }}/>
+
+            </HStack>
 
         </VStack>
     );
---
+++ easyPdf/frontend/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+
+box: {
+    width: '80%',
+    height: 150,
+    backgroundColor: 'red',
+    alignSelf: 'center',
+    borderRadius: 9
+  }
+
+});
+
+export { styles }
