4ea8d58ed16636540be99871cf258a547390dc05
Cristina <stiopucristina@yahoo.com>
2022-11-14 14:21:59 +0200

    too little to be called update

--- easyPdf/frontend/components/home/HomePage.js
+++ easyPdf/frontend/components/home/HomePage.js
@@ -8,23 +8,44 @@ import flex from "react-native-flex-layout/src/Flex";
 
 const HomePage = () => {
     return(
-        <VStack>
-            <Box w='100%' h='35%' style={{ backgroundColor: '#3F4041' }}>
+        <IconComponentProvider IconComponent={MaterialCommunityIcons}>
+            <VStack>
+                <Box w='100%' h='35%' style={{ backgroundColor: '#3F4041' }}>
+                    <VStack>
+                        <HStack>
+                            <IconButton
+                            // onPress={}
+                            icon={props => <Icon name="account-circle-outline" {...props} />}
+                            color="white"
+                            />
+                            <IconButton icon={props => <Icon name="magnify" {...props} />}
 
-            </Box>
-            <Box w='100%' h='65%' style={{ backgroundColor: '#2C2E30' }}>
-                <VStack items="center" spacing='10%' style={ {marginTop:"20%", marginBottom:"20%",  marginHorizontal:"10%",  justifyContent: "center", alignItems: "center", display:"flex"}}>
-                    <Surface
-                      elevation={8}
-                      style={{ width: '100%', height: '50%', backgroundColor: '#3F4041', borderRadius: 10  }}
-                    />
-                    <Surface
-                      elevation={8}
-                      style={{ width: '100%', height: '50%', backgroundColor: '#3F4041', borderRadius: 10 }}
-                    />
-                </VStack>
-            </Box>
-        </VStack>
+                            color="white"
+                            />
+                            <IconButton icon={props => <Icon name="dots-vertical" {...props} />}
+                            color="white"
+                            />
+                        </HStack>
+                        <HStack>
+
+                        </HStack>
+                    </VStack>
+
+                </Box>
+                <Box w='100%' h='65%' style={{ backgroundColor: '#2C2E30' }}>
+                    <VStack items="center" spacing='10%' style={ {marginTop:"20%", marginBottom:"20%",  marginHorizontal:"10%",  justifyContent: "center", alignItems: "center", display:"flex"}}>
+                        <Surface
+                          elevation={8}
+                          style={{ width: '100%', height: '50%', backgroundColor: '#3F4041', borderRadius: 10  }}
+                        />
+                        <Surface
+                          elevation={8}
+                          style={{ width: '100%', height: '50%', backgroundColor: '#3F4041', borderRadius: 10 }}
+                        />
+                    </VStack>
+                </Box>
+            </VStack>
+        </IconComponentProvider>
     )
 }
 
